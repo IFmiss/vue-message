000c21d6706a6829008552cdd4d038c51abc0bf9
daiwei <185098535@qq.com>
2017-11-21 22:37:49 +0800

    提交新版msg的完整版本 1.2.0

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,11 @@
   <div id="app">
     <button @click="defaultMsg">默认提示</button>
     <button @click="zMsg">自定义提示</button>
-    <button @click="bgMsg">背景提示</button>
+    <button @click="bgMsg">背景改变提示</button>
+    <button @click="durMsg">时长控制</button>
+    <button @click="topMsg">显示位置Top</button>
+    <button @click="centerMsg">显示位置Center</button>
+    <button @click="callbackMsg">msg消失之后的回调</button>
   </div>
 </template>
 
@@ -19,20 +23,25 @@ export default {
       this.$msg()
     },
     bgMsg () {
-      this.$msg({background:'red'})
+      this.$msg({background:'radial-gradient(rgba(4,12,6,0.3), #F1DCAB);'})
     },
     zMsg () {
       this.$msg('你好  你好啊!')
     },
-    defaultMsg () {
-      this.$msg()
+    durMsg () {
+      this.$msg({text: '我设置了时长为1000ms',duration: 1000})
     },
-    defaultMsg () {
-      this.$msg()
+    topMsg () {
+      this.$msg({position: 'top'})
+    },
+    centerMsg () {
+      this.$msg({position: 'center'})
+    },
+    callbackMsg () {
+      this.$msg({position: 'center', duration: 1000}, () => {
+        alert('这是Vue-Message结束之后的回调')
+      })
     }
-  },
-  mounted () {
-    // this.$msg({text: 'aaa'})
   }
 }
 </script>
@@ -44,7 +53,39 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  // margin-top: 60px;
+  width:100%;
+  height:100%;
+}
+
+button{
+  padding: 5px 15px;
+  border:none;
+  border-radius:4px;
+  outline:0 none;
+  color:#f0f0f0;
+  margin:10px 5px;
+  background:rgba(233,233,233,0.3);
+  cursor:pointer;
+  &:hover{
+    background:rgba(233,233,233,0.4);
+  }
+}
+
+body,html{
+  min-width: 100vw;
+  min-height: 100vh;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  background: -webkit-linear-gradient(to right, #EF629F, #EECDA3);
+  background: linear-gradient(to right, #EF629F, #EECDA3);
+  font-weight: lighter;
+  font-size: 10px;
+  overflow:hidden;
 }
 
 h1, h2 {
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import VueMessage from './lib/index.js'
-Vue.use(VueMessage)
+Vue.use(VueMessage, {background: 'rgba(0,0,0,0.5)'})
 
 new Vue({
   el: '#app',
